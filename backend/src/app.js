9079d27a6b1a926622d4e6b3245b91c688087197
Giande <gianjaen98@gmail.com>
2023-03-25 05:36:57 -0500

    backend build config w babel

---
+++ backend/src/app.js
@@ -0,0 +1,26 @@
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import { displayModelsAndImages } from "./database.js";
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(cors());
+
+app.get("/api/porsche", async (req, res) => {
+  const data = await displayModelsAndImages();
+  res.json(data);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}...`);
+});
