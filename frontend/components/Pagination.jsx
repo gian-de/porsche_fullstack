c660f3f18ec0c088e6ee68b610245e19ba7dbe84
Giande <gianjaen98@gmail.com>
2023-04-18 10:16:26 -0500

    xl:pl-40 for pagination

--- frontend/components/Pagination.jsx
+++ frontend/components/Pagination.jsx
@@ -9,7 +9,7 @@ const Pagination = ({ pageCount, handlePageChange, currentPage }) => {
         forcePage={currentPage}
         pageRangeDisplayed={5}
         marginPagesDisplayed={1}
-        containerClassName="flex items-center space-x-3 sm:space-x-5 lg:pl-24"
+        containerClassName="flex items-center space-x-3 sm:space-x-5 lg:pl-36 xl:pl-40"
         pageClassName="flex items-center justify-center text-lg rounded-md text-gray-800 hover:bg-black hover:text-slate-100 transition duration-150 ease"
         pageLinkClassName="w-10 h-10 flex items-center justify-center"
         previousClassName="hidden sm:flex p-2 rounded-md text-gray-800 hover:bg-black hover:text-slate-100 transition duration-150 ease"
